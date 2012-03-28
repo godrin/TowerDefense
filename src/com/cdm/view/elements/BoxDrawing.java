9c7cae40fbf1df1f6bb4d6b15c8839b77c1f3d10
David Kamphausen <david.kamphausen@web.de>
2012-03-28 17:23:29 +0200

    better outline

--- src/com/cdm/view/elements/BoxDrawing.java
+++ src/com/cdm/view/elements/BoxDrawing.java
@@ -1,47 +1,38 @@
 package com.cdm.view.elements;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.math.Vector3;
 import com.cdm.view.IRenderer;
 import com.cdm.view.Position;
 import com.cdm.view.Position.RefSystem;
 
 public class BoxDrawing {
-	float size = 2.0f;
-	Color color = new Color(0, 0, 1, 1);
-	Position pos = new Position(0, 0, RefSystem.Level);
-
-	// upper box
-	Vector3 a, b, c, d, e, f, g, h;
-
-	List<Vector3> lines = Arrays.asList(new Vector3[] { a, b, b, c, c, d, e, f,
-			f, g, g, h });
-	float angle = 0.0f;
+	List<SingleBox> boxes = new ArrayList<SingleBox>();
 
 	public BoxDrawing(Position start, Position end, int gh) {
+		for (int x = (int) start.x; x <= (int) end.x; x++) {
+			makeBox(x, -1);
+			makeBox(x, gh + 1);
+		}
+		for (int y = -1; y <= gh; y++) {
+			if (y == (int) start.y)
+				continue;
+			makeBox(-1, y);
+			makeBox((int) end.x, y);
+		}
+	}
 
-		// upper box
-		a = new Vector3(0, start.y - 0.5f, 0);
-		b = new Vector3(0, 0, 0);
-		c = new Vector3(end.x - 0.5f, 0, 0);
-		d = new Vector3(end.x - 0.5f, start.y - 0.5f, 0);
-		// lower box
-		e = new Vector3(0, start.y + 0.5f, 0);
-		f = new Vector3(0, gh - 0.5f, 0);
-		g = new Vector3(end.x - 0.5f, gh - 0.5f, 0);
-		h = new Vector3(end.x - 0.5f, start.y + 0.5f, 0);
-
-		lines = Arrays.asList(new Vector3[] { a, b, b, c, c, d, e, f, f, g, g,
-				h });
-
+	private void makeBox(int x, int y) {
+		SingleBox box;
+		box = new SingleBox();
+		box.setPos(new Position(x - 1, y - 1, RefSystem.Level));
+		boxes.add(box);
 	}
 
 	public void draw(IRenderer renderer) {
 
-		renderer.drawLines(pos, lines, angle, color, size, RefSystem.Level);
-
+		for (int i = 0; i < boxes.size(); i++)
+			boxes.get(i).draw(renderer);
 	}
 }
---
+++ src/com/cdm/view/elements/SingleBox.java
@@ -0,0 +1,47 @@
+package com.cdm.view.elements;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.math.Vector3;
+import com.cdm.view.IRenderer;
+import com.cdm.view.Position;
+import com.cdm.view.Position.RefSystem;
+
+public class SingleBox {
+
+	private Position pos = new Position(0, 0, RefSystem.Level);
+	private static List<Vector3> lines = new ArrayList<Vector3>();
+	private float angle = 0.0f;
+	private Color color = new Color(0.5f, 0.5f, 0.8f, 0.7f);
+	private float size = 2.0f;
+
+	public SingleBox() {
+		if (lines.size() == 0) {
+			float res = 4;
+			Vector3 s=new Vector3(-0.5f,-0.5f,0);
+
+			for (int x = 0; x < res; x++) {
+				lines.add(new Vector3(x / res, 0, 0).sub(s));
+				lines.add(new Vector3(0, x / res, 0).sub(s));
+				lines.add(new Vector3(x / res, 1, 0).sub(s));
+				lines.add(new Vector3(1, x / res, 0).sub(s));
+			}
+		}
+	}
+
+	public Position getPos() {
+		return pos;
+	}
+
+	public void setPos(Position pos) {
+		this.pos = pos;
+	}
+
+	public void draw(IRenderer renderer) {
+
+		renderer.drawLines(pos, lines, angle, color, size, RefSystem.Level);
+
+	}
+}
