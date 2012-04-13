15e0777b3c8b2f22337aaf805553015256365155
David Kamphausen <david.kamphausen@web.de>
2012-04-13 07:36:21 +0200

    nicer rocketshot ?

--- src/com/cdm/view/elements/shots/RocketShot.java
+++ src/com/cdm/view/elements/shots/RocketShot.java
@@ -1,59 +1,44 @@
 package com.cdm.view.elements.shots;
 
-import java.util.Arrays;
-import java.util.List;
-
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.graphics.GL10;
 import com.cdm.view.IRenderer;
+import com.cdm.view.PolySprite;
 import com.cdm.view.Position;
 import com.cdm.view.elements.Level;
 
 public class RocketShot extends MovingShot {
 
 	public static float speed = 1f;
-	private List<Vector3> lines;
-	private List<Vector3> poly;
+	private PolySprite sprite = null;
 
 	public RocketShot(Position from, Position to, Level plevel) {
 		super(from, to, plevel);
-		Vector3 a = new Vector3(0, -0.4f, 0);
-		Vector3 b = new Vector3(0.75f, 0, 0);
-		Vector3 c = new Vector3(0, 0.4f, 0);
-		Vector3 d = new Vector3(0, 0.2f, 0);
-		Vector3 e = new Vector3(-0.5f, 0.2f, 0);
-		Vector3 f = new Vector3(-0.75f, 0.5f, 0);
-		Vector3 g = new Vector3(-0.75f, -0.5f, 0);
-		Vector3 h = new Vector3(-0.5f, -0.2f, 0);
-		Vector3 i = new Vector3(0, -0.2f, 0);
-
-		lines = Arrays.asList(new Vector3[] { a, b, b, c, c, d, d, e, e, f, f,
-				g, g, h, h, i, i, a });
-		poly = Arrays.asList(new Vector3[] { a, b, c, d, e, i, i, h, e, h, g,
-				f, f, g, i, f, g, d });
-
+		if (sprite == null) {
+			sprite = new PolySprite();
+			sprite.fillRectangle(-0.75f, -0.2f, 1.5f, 0.4f, new Color(0.7f,
+					0.5f, 0.2f, 0.9f));
+			sprite.fillRectangle(-0.75f, -0.3f, 0.6f, 0.6f, new Color(0.4f,
+					0.2f, 0.08f, 0.9f));
+			sprite.init();
+		}
 	}
 
 	public float getSize() {
-		return 0.2f;
+		return 0.3f;
 
 	}
 
 	@Override
 	public void draw(IRenderer renderer) {
-
-		renderer.drawPoly(getPosition(), poly, angle,
-				new Color(1, 0, 0, 1.0f), getSize());
-		renderer.drawLines(getPosition(), lines, angle,
-				new Color((float) Math.random() + 1, (float) Math.random(),
-						(float) Math.random(), 1.0f), getSize());
+		renderer.render(sprite, pos, getSize(), angle, GL10.GL_TRIANGLES);
 	}
 
 	@Override
 	public float getSpeed() {
 		return speed;
 	}
-	
+
 	public float getLevel() {
 		return 3;
 	}
