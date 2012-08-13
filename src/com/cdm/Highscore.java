73258310d301e78f4a2ff4afae21c3dcac87b0b6
David Kamphausen <david.kamphausen@web.de>
2012-08-13 17:20:30 +0200

    support highscore tables len!=10

--- src/com/cdm/Highscore.java
+++ src/com/cdm/Highscore.java
@@ -1,121 +1,15 @@
 package com.cdm;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.util.ArrayList;
 import java.util.List;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.json.JSONTokener;
+import com.cdm.HighscoreServer.Entry;
+import com.cdm.HighscoreServer.HighscoreAccessException;
 
-import com.cdm.view.Renderer;
+public interface Highscore {
 
-public class Highscore {
-	private static final int BUFSIZE = 1024 * 100;
-	private String url = "http://0.0.0.0:9292";
-	private Renderer renderer = new Renderer();
+	public abstract List<Entry> post(Entry entry)
+			throws HighscoreAccessException;
 
-	public static class HighscoreAccessException extends Exception {
+	public abstract List<Entry> read() throws HighscoreAccessException;
 
-		public HighscoreAccessException(Exception e) {
-			super(e);
-		}
-
-		/**
-		 * 
-		 */
-		private static final long serialVersionUID = -4486188937322255607L;
-
-	}
-
-	public static class Entry {
-
-		Entry(String pName, Integer pValue) {
-			name = pName;
-			value = pValue;
-		}
-
-		public String name;
-		public Integer value;
-		
-		public String toString() {
-			String s=name;
-			while(s.length()<20)
-				s+=" ";
-			s+=value;
-			while(s.length()<24)
-				s+=" ";
-			return s;
-		}
-	}
-
-	public static List<Entry> main(String[] args) throws HighscoreAccessException {
-		Highscore s = new Highscore();
-		Entry n = new Entry("HI", 999);
-		List<Entry> vals = s.post(n);
-		System.out.println(vals);
-		return vals;
-		
-	}
-
-	public void setUrl(String pUrl) {
-		url = pUrl;
-	}
-
-	public List<Entry> post(Entry entry) throws HighscoreAccessException {
-		try {
-			String curl = url + "/save?score=" + entry.value + "&name="
-					+ URLEncoder.encode(entry.name, "utf8");
-			InputStream response = new URL(curl).openStream();
-
-			return readFromInputStream(response);
-
-		} catch (Exception e) {
-			throw new HighscoreAccessException(e);
-		}
-
-	}
-
-	public List<Entry> read() throws HighscoreAccessException {
-		try {
-			InputStream response = new URL(url).openStream();
-
-			return readFromInputStream(response);
-
-		} catch (Exception e) {
-			throw new HighscoreAccessException(e);
-		}
-
-	}
-
-	private List<Entry> readFromInputStream(InputStream response)
-			throws IOException, JSONException {
-		List<Entry> scores = new ArrayList<Entry>();
-		InputStreamReader isr = new InputStreamReader(response);
-
-		char[] buf = new char[BUFSIZE];
-		int len = isr.read(buf, 0, BUFSIZE);
-
-		StringBuilder builder = new StringBuilder();
-		builder.append(buf, 0, len);
-		JSONTokener tokener = new JSONTokener(builder.toString());
-		JSONArray finalResult = new JSONArray(tokener);
-		for (int i = 0; i < finalResult.length(); i++) {
-			Object o = finalResult.get(i);
-			if (o instanceof JSONObject) {
-				JSONObject jo = (JSONObject) o;
-				String name = jo.getString("name");
-				Integer value = jo.getInt("value");
-				scores.add(new Entry(name, value));
-			}
-		}
-		isr.close();
-		response.close();
-		return scores;
-	}
 }
--- src/com/cdm/view/HighScoreScreen.java
+++ src/com/cdm/view/HighScoreScreen.java
@@ -6,8 +6,9 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.cdm.Game;
 import com.cdm.Highscore;
-import com.cdm.Highscore.Entry;
-import com.cdm.Highscore.HighscoreAccessException;
+import com.cdm.HighscoreServer;
+import com.cdm.HighscoreServer.Entry;
+import com.cdm.HighscoreServer.HighscoreAccessException;
 import com.cdm.gui.BigButton;
 import com.cdm.gui.IButtonPressed;
 import com.cdm.gui.WidgetContainer;
@@ -18,6 +19,7 @@ public class HighScoreScreen extends Screen implements IButtonPressed {
 	private Renderer renderer = new Renderer();
 	private Game game;
 	private List<Entry> vals;
+	private Highscore highscoreServer;
 	Color white = new Color(1, 1, 1, 1);
 
 	public HighScoreScreen(Game pgame) {
@@ -25,25 +27,29 @@ public class HighScoreScreen extends Screen implements IButtonPressed {
 		game = pgame;
 		gui.add(new BigButton(Gdx.graphics.getWidth() / 2, 100, Gdx.graphics
 				.getWidth() / 6, 50, "back", "back", this));
-		try {
-			vals = Highscore.main(null);
-		} catch (HighscoreAccessException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		highscoreServer = new HighscoreServer();
 	}
 
 	@Override
 	public void render(float delta) {
+		if (vals == null) {
+			try {
+				vals = highscoreServer.read();
+			} catch (HighscoreAccessException e) {
+			}
+		}
 		super.render(delta);
 		gui.addTime(delta);
 		gui.draw(renderer);
 		renderer.drawText(150, 480, "Highscores:", white, 3);
-		renderer.drawText(150, 350, "" + vals.get(0) + "\n" + vals.get(1)
-				+ "\n" + vals.get(2) + "\n" + vals.get(3) + "\n" + vals.get(4)
-				+ "\n" + vals.get(5) + "\n" + vals.get(6) + "\n" + vals.get(7)
-				+ "\n" + vals.get(8) + "\n" + vals.get(9) + "\n", white);
-
+		if (vals != null) {
+			StringBuilder sb = new StringBuilder();
+			for (Entry entry : vals) {
+				sb.append(entry.toString());
+				sb.append("\n");
+			}
+			renderer.drawText(150, 350, sb.toString(), white);
+		}
 	}
 
 	@Override
