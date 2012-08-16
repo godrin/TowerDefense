1ac5691c0d83ccd39d0928948377d0cdf4ecdf70
undermink <undermink@devtal.de>
2012-08-16 22:43:52 +0200

    input screen

---
+++ src/com/cdm/view/InputScreen.java
@@ -0,0 +1,101 @@
+package com.cdm.view;
+
+import java.util.List;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.cdm.Game;
+import com.cdm.Highscore;
+import com.cdm.HighscoreServer;
+import com.cdm.HighscoreServer.Entry;
+import com.cdm.HighscoreServer.HighscoreAccessException;
+import com.cdm.gui.BigButton;
+import com.cdm.gui.IButtonPressed;
+import com.cdm.gui.WidgetContainer;
+import com.cdm.view.elements.Level;
+
+public class InputScreen extends Screen implements IButtonPressed {
+
+	private WidgetContainer gui = new WidgetContainer();
+	private Renderer renderer = new Renderer();
+	private Level level;
+	private Game game;
+	private List<Entry> vals;
+	private Highscore highscoreServer;
+	public String name = "";
+	private int points;
+	Color white = new Color(1, 1, 1, 1);
+
+	public InputScreen(Game pgame, Campaign c) {
+
+		game = pgame;
+		points=c.playerState.getPoints();
+		gui.add(new BigButton(Gdx.graphics.getWidth() / 2, 50, Gdx.graphics
+				.getWidth() / 3, 50, "commit", "commit", this));
+		highscoreServer = new HighscoreServer();
+
+	}
+	public boolean keyTyped (char character) {
+	 char c = character;
+	 name+= c;
+	 return false;
+	}
+	
+	@Override
+	public void render(float delta) {
+		if (vals == null) {
+			try {
+				vals = highscoreServer.read();
+			} catch (HighscoreAccessException e) {
+			}
+		}
+		super.render(delta);
+		gui.addTime(delta);
+		gui.draw(renderer);
+		renderer.drawText(150, 480, "ENTER YOUR NAME:", white, 2);
+		renderer.drawText(250, 300,name, white,2);
+		if (vals != null) {
+			StringBuilder sb = new StringBuilder();
+			for (Entry entry : vals) {
+				sb.append(entry.toString());
+				sb.append(" Points\n");
+			}
+			//renderer.drawText(110, 380, sb.toString(), white,1.25f);
+		}
+	}
+
+	@Override
+	public void buttonPressed(String buttonName) {
+		if (buttonName.equals("commit")){
+		
+			Entry n = new Entry(name, points );
+			try {
+				highscoreServer.post(n);
+			} catch (HighscoreAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			name="";
+			game.setScreen(Screen.HIGHSCORE_SCREEN);
+			}
+	}
+
+	public boolean touchDown(int x, int y, int pointer, int button) {
+		y = Gdx.graphics.getHeight() - y;
+		if (gui.opaque(x, y)) {
+			gui.touchDown(x, y, pointer, button);
+			return true;
+		}
+		return false;
+	}
+
+	public boolean touchUp(int x, int y, int pointer, int button) {
+		y = Gdx.graphics.getHeight() - y;
+		if (gui.opaque(x, y)) {
+			gui.touchUp(x, y, pointer, button);
+			return true;
+		}
+		return false;
+	}
+
+}
